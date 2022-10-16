32f8c19036d9e7ee69b3960e654ee09d9643e5a2
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2022-10-16 20:31:21 +0300

    16/10/2022/20/31

--- app/src/main/java/com/sergeygovorunov/imagecollection/MainActivity.java
+++ app/src/main/java/com/sergeygovorunov/imagecollection/MainActivity.java
@@ -9,9 +9,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
 
 import com.google.android.material.navigation.NavigationView;
 import com.sergeygovorunov.imagecollection.adapters.CollectionListViewAdapter;
@@ -26,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rv_collection_list;
     private RecyclerView rv_file_list;
     private DrawerLayout main_drawer_layout;
+    private ImageSwitcher image_switcher;
 
     private CollectionListViewAdapter collectionListViewAdapter;
     private FileListViewAdapter fileListViewAdapter;
@@ -34,12 +49,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
         fileListViewAdapter = new FileListViewAdapter(this);
         fileListViewAdapter.setOnItemClickListener((item, position) -> {
-
+            Bitmap bitmapOrig = BitmapFactory.decodeFile(item.getPath());
+            double w = bitmapOrig.getWidth();
+            double h = bitmapOrig.getHeight();
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            double nw = displayMetrics.widthPixels;
+            double nh = displayMetrics.heightPixels;
+            if (w > h) {
+                nh = nw / (w / h);
+            } else {
+                nw = nh / (h / w);
+            }
+            Bitmap bitmap = Bitmap.createScaledBitmap(bitmapOrig, (int) nw, (int) nh, false);
+            Drawable drawable = new BitmapDrawable(bitmap);
+            image_switcher.setImageDrawable(drawable);
         });
         rv_file_list = findViewById(R.id.file_list);
         rv_file_list.setAdapter(fileListViewAdapter);
+        //
         collectionListViewAdapter = new CollectionListViewAdapter(this);
         main_drawer_layout = findViewById(R.id.main_drawer_layout);
         collectionListViewAdapter.setOnCollectionChangedListener((collection, position) -> {
@@ -48,6 +79,23 @@ public class MainActivity extends AppCompatActivity {
         });
         rv_collection_list = findViewById(R.id.collection_list);
         rv_collection_list.setAdapter(collectionListViewAdapter);
+        //
+        image_switcher = findViewById(R.id.image_switcher);
+        Animation inAnim = new TranslateAnimation(0, 100, 0, 0);
+        inAnim.setDuration(1000);
+        image_switcher.setInAnimation(inAnim);
+        Animation outAnim = new TranslateAnimation(-100, 0, 0, 0);
+        outAnim.setDuration(1000);
+        image_switcher.setOutAnimation(outAnim);
+        image_switcher.setFactory(() -> {
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setLayoutParams(new ImageSwitcher.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            imageView.setBackgroundColor(0xFFFFFFFF);
+            return imageView;
+        });
+        //
         directoryChooser = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == Activity.RESULT_OK) {
                 Bundle bundle = result.getData().getExtras();
